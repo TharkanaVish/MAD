b796265ed1411038f2aea32bc8047c7f1b8ec114
TharkanaVish <tharkana.vish@gmail.com>
2020-08-08 22:35:24 +0530

    Initial tutorial 2 commitment

---
+++ app/src/main/java/com/example/usingstrings/ActivityOne.java
@@ -0,0 +1,18 @@
+package com.example.usingstrings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ActivityOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+
+        TextView txtView2 = findViewById(R.id.textView2);
+        txtView2.setText(R.string.Msg2);
+    }
+}
